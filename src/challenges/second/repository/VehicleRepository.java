bbce440bc2f0ea45e4e774e93299836a8109dc14
wemmelotec <tonmelo@hotmail.com>
2022-01-23 19:57:17 -0300

    implemente service

--- src/challenges/second/repository/VehicleRepository.java
+++ src/challenges/second/repository/VehicleRepository.java
@@ -4,7 +4,7 @@ import challenges.second.classes.Vehicle;
 
 public class VehicleRepository {
 
-     private Vehicle[] bancoDeDadosVehicle = new Vehicle[6];
+     private Vehicle[] bancoDeDadosVehicle = new Vehicle[12];
 
      public void createBancoDeDados(){
           Automaker automaker = new Automaker("GM");
@@ -14,17 +14,30 @@ public class VehicleRepository {
           Automaker automaker5 = new Automaker("Mercedes");
           Automaker automaker6 = new Automaker("Peugeot");
           Vehicle vehicle = new Vehicle(automaker, "Suburban","back",2020);
-          Vehicle vehicle2 = new Vehicle(automaker2,"Azera", "blue", 2022);
-          Vehicle vehicle3 = new Vehicle(automaker3,"Golf", "red", 2015);
-          Vehicle vehicle4 = new Vehicle(automaker4,"A4", "yellow", 2019);
-          Vehicle vehicle5 = new Vehicle(automaker5,"C 180", "white", 2010);
-          Vehicle vehicle6 = new Vehicle(automaker6,"206", "red", 2008);
+          Vehicle vehicle2 = new Vehicle(automaker, "Suburban","back",2020);
+          Vehicle vehicle3 = new Vehicle(automaker, "Suburban","back",2020);
+          Vehicle vehicle4 = new Vehicle(automaker2,"Azera", "blue", 2022);
+          Vehicle vehicle5 = new Vehicle(automaker2,"Azera", "blue", 2022);
+          Vehicle vehicle6 = new Vehicle(automaker2,"Azera", "blue", 2022);
+          Vehicle vehicle7 = new Vehicle(automaker3,"Golf", "red", 2015);
+          Vehicle vehicle8 = new Vehicle(automaker4,"A4", "yellow", 2019);
+          Vehicle vehicle9 = new Vehicle(automaker5,"C 180", "white", 2010);
+          Vehicle vehicle10 = new Vehicle(automaker6,"206", "red", 2008);
+          Vehicle vehicle11 = new Vehicle(automaker6,"206", "red", 2008);
+          Vehicle vehicle12 = new Vehicle(automaker6,"206", "red", 2008);
+
           bancoDeDadosVehicle[0]=vehicle;
           bancoDeDadosVehicle[1]=vehicle2;
           bancoDeDadosVehicle[2]=vehicle3;
           bancoDeDadosVehicle[3]=vehicle4;
           bancoDeDadosVehicle[4]=vehicle5;
           bancoDeDadosVehicle[5]=vehicle6;
+          bancoDeDadosVehicle[6]=vehicle7;
+          bancoDeDadosVehicle[7]=vehicle8;
+          bancoDeDadosVehicle[8]=vehicle9;
+          bancoDeDadosVehicle[9]=vehicle10;
+          bancoDeDadosVehicle[10]=vehicle11;
+          bancoDeDadosVehicle[11]=vehicle12;
      }
 
      public VehicleRepository() {
--- src/challenges/second/service/VehicleService.java
+++ src/challenges/second/service/VehicleService.java
@@ -3,16 +3,35 @@ package challenges.second.service;
 import challenges.second.classes.Vehicle;
 import challenges.second.repository.VehicleRepository;
 
+import java.util.Arrays;
+
 public class VehicleService {
 
     VehicleRepository vehicleRepository = new VehicleRepository();
+
+    public static String[] retiraRepetidos(String[] array) {
+        Arrays.sort(array);
+        String[] lista = new String[array.length];
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            if ((i > 0) && (array[i].equals(array[i - 1])) && (array[i].equals(array[i + 1]))) continue;
+            if ((i < array.length - 2) && (array[i].equals(array[i + 2]))) continue;
+            lista[j++] = array[i];
+        }
+        if (j != array.length){
+            String[] newLista = new String[j];
+            System.arraycopy(lista, 0, newLista, 0, j);
+            return newLista;
+        }
+        return lista;
+    }
 
     public String[] searchByAutomaker(){
         String[] automakers = new String[vehicleRepository.getBancoDeDadosVehicle().length];
         for (int i = 0; i < vehicleRepository.getBancoDeDadosVehicle().length ; i++) {
             automakers[i] = vehicleRepository.getBancoDeDadosVehicle()[i].getAutomaker().getName();
         }
-        return automakers;
+        return retiraRepetidos(automakers);
     }
 
     public VehicleRepository getVehicleRepository() {
