fe00b5e8b6fedd9ec5f311b3f4e7c379a97ea746
wemmelotec <tonmelo@hotmail.com>
2022-01-23 08:16:21 -0300

    listagem das montadoras ok

--- src/challenges/second/Application.java
+++ src/challenges/second/Application.java
@@ -1,7 +1,10 @@
 package challenges.second;
+
 import challenges.second.classes.Vehicle;
 import challenges.second.repository.VehicleRepository;
 import challenges.second.service.VehicleService;
+
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
@@ -10,12 +13,44 @@ public class Application {
 //        for (Vehicle x: vehicleRepository.getBancoDeDadosVehicle()) {
 //            System.out.println(x.getAutomaker());
 //        }
+        int seletor;
+        Scanner opcaoDoUsuario = new Scanner(System.in);
         VehicleService vehicleService = new VehicleService();
-        vehicleService.searchByAutomaker();
 
-//        for (String x: vehicleService.searchByAutomaker()) {
-//            System.out.println();
-//        }
+        do {
+            System.out.println("       -- Menu --\n");
+            System.out.println("1 - Search by automaker");
+            System.out.println("0 - Exit");
+            System.out.print("\n" + "Choose one of the options: ");
+            seletor = opcaoDoUsuario.nextInt();
+            //implementar uma limpeza de tela aqui
+            if (seletor == 1) {
+                switch (seletor) {
+                    case 1:
+                        int numeroColunaDaEsquerda = 1;
+                        int seletorDoMenuMontadora;
+                        for (String x : vehicleService.searchByAutomaker()) {
+                            System.out.printf("%d - " + x + "\n", numeroColunaDaEsquerda );
+                            numeroColunaDaEsquerda++;
+                        }
+
+                        System.out.print("\n" +"Choose one of the options: ");
+                        seletorDoMenuMontadora = opcaoDoUsuario.nextInt();
+
+                        //aqui implemento a busca pela montadora
+                        //if equals ignore case == w sout
+                        vehicleService.getVehicleRepository().getBancoDeDadosVehicle()[seletorDoMenuMontadora-1].getAutomaker().getName();
+
+                        break;
+                    default:
+                        System.out.println("Invalid option!\n");
+                }
+                System.out.println("Do you wish to continue?");
+                System.out.println("0 - No\n" + "1 - Yes");
+                seletor = opcaoDoUsuario.nextInt();
+            }
+
+        } while (seletor == 1);
 
     }
 }
--- src/challenges/second/service/VehicleService.java
+++ src/challenges/second/service/VehicleService.java
@@ -1,20 +1,25 @@
 package challenges.second.service;
 
+import challenges.second.classes.Vehicle;
 import challenges.second.repository.VehicleRepository;
 
 public class VehicleService {
 
     VehicleRepository vehicleRepository = new VehicleRepository();
 
-    //ele tem que retornar um novo array com os veiculos das montadoras
-    public void searchByAutomaker(){
-
+    public String[] searchByAutomaker(){
+        String[] automakers = new String[vehicleRepository.getBancoDeDadosVehicle().length];
+        for (int i = 0; i < vehicleRepository.getBancoDeDadosVehicle().length ; i++) {
+            automakers[i] = vehicleRepository.getBancoDeDadosVehicle()[i].getAutomaker().getName();
+        }
+        return automakers;
     }
+
     public VehicleRepository getVehicleRepository() {
         return vehicleRepository;
     }
 
-    public void setVehicleRepository(VehicleRepository vehicleRepository) {
-        this.vehicleRepository = vehicleRepository;
-    }
+//    public void setVehicleRepository(VehicleRepository vehicleRepository) {
+//        this.vehicleRepository = vehicleRepository;
+//    }
 }
